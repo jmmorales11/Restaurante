9ee2c76bdfc0d08eb731607d9ce4af9418c355a6
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2022-07-11 21:12:40 -0500

    espero no se dañe :c

---
+++ RestauranteApp/src/modelo/Conexion.java
@@ -0,0 +1,22 @@
+
+package modelo;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+public class Conexion {
+        DB baseDatos;
+    DBCollection coleccion;
+    DBCollection coleccionPedido;
+    
+    //Constructor
+    public Conexion(){
+        MongoClient mongo=new MongoClient("localhost",27017);
+        baseDatos= mongo.getDB("Restaurante");
+        coleccion= baseDatos.getCollection("Usuario");
+        coleccionPedido=baseDatos.getCollection("Pedidos");
+        System.out.println("Conexion aBD ok");
+    }
+}
+
