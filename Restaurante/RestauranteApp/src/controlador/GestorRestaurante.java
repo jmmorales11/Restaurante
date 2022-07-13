02a5e0a599eadfd15d3b456908b06f9f0a41e678
Camilo Andrés Orrico Emme <99036443+caorrico@users.noreply.github.com>
2022-07-13 10:31:54 -0500

    -.-

--- Restaurante/RestauranteApp/src/controlador/GestorRestaurante.java
+++ Restaurante/RestauranteApp/src/controlador/GestorRestaurante.java
@@ -29,101 +29,14 @@ import vista.FrmVisualisarInformacionPedido;
 public class GestorRestaurante {
     //Camilo 
      public static void main(String args[]){
+        FrmRegistrar r1 = new FrmRegistrar();
+        FrmLogin l1 = new FrmLogin();
+        FrmEscogeTuSabor menu1= new FrmEscogeTuSabor();
+        UsuarioDAO dao = new UsuarioDAO();
+        ControladorRestaurante c3= new ControladorRestaurante(menu1);
+        ControladorRestaurante c1= new ControladorRestaurante(r1,dao);
+        ControladorRestaurante c2= new ControladorRestaurante(l1,dao);
 
-        FrmBebidas bebidas= new FrmBebidas();
-        FrmFuerte fuerte= new FrmFuerte();
-        FrmPostre postres= new FrmPostre();
-        FrmConfirmacion confirmacion =new FrmConfirmacion();
-        FrmEntradas entradas= new FrmEntradas();
-        FrmMenu menu1= new FrmMenu();
-        PedidosDAO ped= new PedidosDAO();
-        ControladorRestaurante c= new ControladorRestaurante(bebidas,ped);//Marley       
-        ControladorRestaurante c1= new ControladorRestaurante(fuerte,ped);//Marley      
-        ControladorRestaurante c3= new ControladorRestaurante(postres,ped);//Marley
-        ControladorRestaurante c2= new ControladorRestaurante(entradas,ped);//Marley
-        ControladorRestaurante c4= new ControladorRestaurante(confirmacion,ped);//Marley
-        
-//        menu1.setVisible(true);
-////entradas.setVisible(true);
-//        confirmacion.setVisible(false);
-//
-////        FrmBebidas bebidas= new FrmBebidas();
-//        FrmFuerte fuerte= new FrmFuerte();
-////        FrmPostre postres= new FrmPostre();
-////        FrmConfirmacion confirmacion =new FrmConfirmacion();
-////        FrmEntradas entradas= new FrmEntradas();
-//        PedidosDAO ped= new PedidosDAO();
-////        ControladorRestaurante c= new ControladorRestaurante(bebidas,ped);       
-//        ControladorRestaurante c1= new ControladorRestaurante(fuerte,ped);       
-//        ControladorRestaurante c3= new ControladorRestaurante(postres,ped);
-//        ControladorRestaurante c2= new ControladorRestaurante(entradas,ped);
-        //entradas.setVisible(true);
-        //confirmacion.setVisible(true);
-
-        fuerte.setVisible(true);
-        //bebidas.setVisible(true);
-//        postres.setVisible(true);
-
-        //fuerte.setVisible(true);
-        bebidas.setVisible(false);
-       // postres.setVisible(true);
-
-        //f1.setVisible(true);
-        //datoQuemados();//kleber
-//f1.setVisible(true);
-
-        
-        FrmRegistrar r1= new FrmRegistrar();
-        FrmVisualisarInformacionPedido rv = new FrmVisualisarInformacionPedido();//kleber
-        ControladorVisualisarIngredientes cVerin =new ControladorVisualisarIngredientes(rv);//kleber
-        UsuarioDAO pDAO = new UsuarioDAO();
-        ControladorUsuario cUsuario= new ControladorUsuario(r1,pDAO);
-//        r1.setVisible(true);
-
-
-//        FrmRegistrar r1= new FrmRegistrar();//Camilo
-//        FrmLogin l1= new FrmLogin();
-//        FrmVisualisarInformacionPedido rv = new FrmVisualisarInformacionPedido();//kleber
-//        ControladorVisualisarIngredientes cVerin =new ControladorVisualisarIngredientes(rv);//kleber
-//        UsuarioDAO pDAO = new UsuarioDAO();
-//        FrmEscogeTuSabor menu = new FrmEscogeTuSabor();
-//
-//        ControladorRestaurante c4= new ControladorRestaurante(r1,pDAO);//Camilo
-//        ControladorRestaurante c5= new ControladorRestaurante(l1,pDAO);//Camilo
-//        ControladorRestaurante c6= new ControladorRestaurante(menu);//Camilo
-//        menu.setVisible(true);//Camilo
-
-//        FrmRegistrar r1= new FrmRegistrar();//Camilo
-//        FrmLogin l1= new FrmLogin();
-//        FrmVisualisarInformacionPedido rv = new FrmVisualisarInformacionPedido();//kleber
-//        ControladorVisualisarIngredientes cVerin =new ControladorVisualisarIngredientes(rv);//kleber
-//        UsuarioDAO pDAO = new UsuarioDAO();
-//        FrmEscogeTuSabor menu = new FrmEscogeTuSabor();
-//
-//        ControladorRestaurante cUsuario= new ControladorRestaurante(r1,pDAO);//Camilo
-//        ControladorRestaurante cUsuario1= new ControladorRestaurante(l1,pDAO);//Camilo
-//        //menu.setVisible(true);//Camilo
-//        //        r1.setVisible(true);
-//        //        r1.setLocationRelativeTo(r1);
-//
-//
-//        ControladorRestaurante c4= new ControladorRestaurante(r1,pDAO);//Camilo
-//        ControladorRestaurante c5= new ControladorRestaurante(l1,pDAO);//Camilo
-//        ControladorRestaurante c6= new ControladorRestaurante(menu);//Camilo
-//        menu.setVisible(true);//Camilo
-
-
-        r1.setVisible(true);
-//        r1.setLocationRelativeTo(r1);
-
-//         FrmBebidas fBebidas=new FrmBebidas();
-//         FrmConfirmacion fConfirmacion= new FrmConfirmacion();
-//         ControladorRestaurante cRestaurante= new ControladorRestaurante();
-//         fBebidas.setVisible(true);
-//         fConfirmacion.setVisible(true);
-
-     }
-
-    
-
+        menu1.setVisible(true);
+    }
 }
